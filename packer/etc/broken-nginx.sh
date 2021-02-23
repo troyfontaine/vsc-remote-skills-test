f018735d66e5ac70d3136bfea1a970b5706d227d
Troy Fontaine <tfontaine@troyfontaine.com>
2021-02-23 00:17:43 -0600

    Signed-off-by: Troy Fontaine <tfontaine@troyfontaine.com> VSCRST-6 - Initial build with Packer
    Adds initial build of nginx test via Packer

---
+++ packer/etc/broken-nginx.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -x
+
+# Install nginx
+sudo apt-get -y -qq install nginx
+
+# Stop and disable nginx
+sudo systemctl stop nginx
+sudo systemctl disable nginx
+
+# Create alternative root directory for Nginx to use
+sudo mkdir -p /web/html/my-app-2/
+sudo chown www-data:www-data /web/
+sudo mv /tmp/default /etc/nginx/sites-available/default
+sudo mv /tmp/index.nginx-debian.html /web/html/my-app-2/index.nginx-debian.html
